abd441e1191f955cd9243331473cb59c69e8641c
whoisYoges <contact.yogesh@pm.me>
2022-02-20 12:57:20 +0545

    custom settings applied

--- config.def.h
+++ config.def.h
@@ -6,7 +6,7 @@ static int centered = 0;                    /* -c option; centers dmenu on scree
 static int min_width = 500;                    /* minimum width when centered */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"monospace:size=10"
+	"Comic Sans MS:size=10"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
