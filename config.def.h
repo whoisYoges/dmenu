59a079689e9468cd9c56cb3e71f1abba3dfe86c0
whoisYoges <helloyogesh@tuta.io>
2022-11-16 14:21:45 +0545

    Applied custom settings

--- config.def.h
+++ config.def.h
@@ -3,17 +3,17 @@
 
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 static int centered = 1;                    /* -c option; centers dmenu on screen */
-static int min_width = 250;                    /* minimum width when centered */
+static int min_width = 200;                    /* minimum width when centered */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"monospace:size=10"
+	"Comic Sans MS:size=12"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
-	/*     fg         bg       */
-	[SchemeNorm] = { "#bbbbbb", "#222222" },
-	[SchemeSel] = { "#eeeeee", "#005577" },
-	[SchemeOut] = { "#000000", "#00ffff" },
+				/*     fg         bg       */
+	[SchemeNorm] = { "#08FF08", "#242424" },
+	[SchemeSel] = { "#FFFF00", "#121212" },
+	[SchemeOut] = { "#222222", "#00ffff" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 5;
@@ -25,4 +25,4 @@ static unsigned int lines      = 5;
 static const char worddelimiters[] = " ";
 
 /* Size of the window border */
-static const unsigned int border_width = 5;
+static const unsigned int border_width = 2;
