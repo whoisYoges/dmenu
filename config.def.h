01fa09703116f8c54be0c583ffd7ba1d5a15d3b1
whoisYoges <helloyogesh@tuta.io>
2022-11-16 13:58:12 +0545

    Original dmenu 5.2

--- config.def.h
+++ config.def.h
@@ -2,11 +2,9 @@
 /* Default settings; can be overriden by command line. */
 
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
-static int centered = 0;                    /* -c option; centers dmenu on screen */
-static int min_width = 500;                    /* minimum width when centered */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"Comic Sans MS:size=10"
+	"monospace:size=10"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
@@ -23,6 +21,3 @@ static unsigned int lines      = 0;
  * for example: " /?\"&[]"
  */
 static const char worddelimiters[] = " ";
-
-/* Size of the window border */
-static const unsigned int border_width = 5;
