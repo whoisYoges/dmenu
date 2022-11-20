c60ed6713fb633283f0736cb097dddf6a325ac86
whoisYoges <helloyogesh@tuta.io>
2022-11-20 13:41:53 +0545

    Added font for color emoji support

--- config.def.h
+++ config.def.h
@@ -6,7 +6,7 @@ static int centered = 1;                    /* -c option; centers dmenu on scree
 static int min_width = 200;                    /* minimum width when centered */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"Comic Sans MS:size=12"
+	"Comic Sans MS:size=12", "Noto Color Emoji:size=12" 
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
