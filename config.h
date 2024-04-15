495f49d2f255533f9c0f97d21828efab3c95dd3e
Castor <whoisyoges@castorisdead.xyz>
2024-04-15 14:28:58 +0545

    Applied my custom configurations.

---
+++ config.h
@@ -0,0 +1,29 @@
+/* See LICENSE file for copyright and license details. */
+/* Default settings; can be overriden by command line. */
+
+static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
+static int centered = 1;                    /* -c option; centers dmenu on screen */
+static int min_width = 200;                    /* minimum width when centered */
+/* -fn option overrides fonts[0]; default X11 font or font set */
+static const char *fonts[] = {
+	"Comic Sans MS:size=12",
+	"Noto Color Emoji:size=12"
+};
+static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
+static const char *colors[SchemeLast][2] = {
+				/*     fg         bg       */
+	[SchemeNorm] = { "#ebdbb2", "#3c3836" },
+	[SchemeSel] = { "#b8bb26", "#282828" },
+	[SchemeOut] = { "#fbf1c7", "#1d2021" },
+};
+/* -l option; if nonzero, dmenu uses vertical list with given number of lines */
+static unsigned int lines      = 5;
+
+/*
+ * Characters not considered part of a word while deleting words
+ * for example: " /?\"&[]"
+ */
+static const char worddelimiters[] = " ";
+
+/* Size of the window border */
+static unsigned int border_width = 2;
